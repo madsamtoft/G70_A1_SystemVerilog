526533301931678d342d55ab006ee378db282ef9
Mads A. Pedersen <mads.amtoft@hotmail.com>
2026-09-24 11:36:02 +0200

    Works as intended

--- Vivado/rtl/vhdl/gcd.vhd
+++ Vivado/rtl/vhdl/gcd.vhd
@@ -43,74 +43,71 @@ begin
 
   -- Combinatoriel logic
 
-  cl : process (req,ab,state,reg_a,reg_b,reset)
+  cl : process(all)
   begin
-  
-    -- Defaults
+    -- Register defaults
     next_reg_a <= reg_a;
     next_reg_b <= reg_b;
     next_state <= state;
 
+    -- Output defaults
+    ack <= '0';
+    C   <= reg_a;
+
     case state is
-        when op1_await =>
-            ack <= '0';
-            if req = '1' then
-                ack <= '1';
-                next_reg_a <= ab;
-                next_state <= hs_reset;
-            end if;
-        when hs_reset =>
-            if req = '0' then
-                ack <= '0';
-                next_state <= op2_await;
-            end if;
-        when op2_await =>
-            if req = '1' then
-                next_reg_b <= ab;
-                next_state <= calc;
-            end if;
-        when calc => 
-            if reg_a > reg_b then
-                next_reg_a <= reg_a - reg_b;
-            elsif reg_a < reg_b then
-                next_reg_b <= reg_b - reg_a;
-            else
-                C <= next_reg_a;
-                ack <= '1';
-                next_state <= read_await;
-            end if;
-        when read_await =>
-            if req = '0' then 
-                ack <= '0';
-                next_state <= op1_await;
-            end if;
+      when op1_await =>
+        if req = '1' and ab /= (ab'range => '0') then
+          ack        <= '1';
+          next_reg_a <= ab;
+          next_state <= hs_reset;
+        end if;
+
+      when hs_reset =>
+        ack <= '1';
+        if req = '0' then
+          ack        <= '0';
+          next_state <= op2_await;
+        end if;
+
+      when op2_await =>
+        if req = '1' and ab /= (ab'range => '0') then
+          next_reg_b <= ab;
+          next_state <= calc;
+        end if;
+
+      when calc =>
+        if reg_a > reg_b then
+          next_reg_a <= reg_a - reg_b;
+        elsif reg_a < reg_b then
+          next_reg_b <= reg_b - reg_a;
+        else
+          C          <= reg_a;
+          ack        <= '1';
+          next_state <= read_await;
+        end if;
+
+      when read_await =>
+        ack <= '1';
+        if req = '0' then
+          ack        <= '0';
+          next_state <= op1_await;
+        end if;
     end case;
-  end process cl;
+  end process;
 
   -- Registers
-
-  seq : process (clk, reset)
+  seq : process (clk)
   begin
-    
-    if reset = '1' then
+    if rising_edge(clk) then
+      if reset = '1' then
         state <= op1_await;
-    elsif rising_edge(clk) then
-        case state is
-            when op1_await =>
-                reg_a <= next_reg_a;
-                state <= next_state;
-            when hs_reset  => 
-                state <= next_state;
-            when op2_await =>
-                reg_b <= next_reg_b;
-                state <= next_state;
-            when calc => 
-                reg_a <= next_reg_a;
-                reg_b <= next_reg_b;
-                state <= next_state;
-            when read_await =>
-                state <= next_state;
-        end case;
+        reg_a <= (others => '0');
+        reg_b <= (others => '0');
+      else
+        state <= next_state;
+        reg_a <= next_reg_a;
+        reg_b <= next_reg_b;
+      end if;
     end if;
   end process seq;
 
