0c2612d0fa13b80398eb5844dc79d2871f2e57cf
Mads A. Pedersen <mads.amtoft@hotmail.com>
2026-09-24 10:23:33 +0200

    Added basic given vhdl files and mac .Docker environment

---
+++ vhdl/task2/gcd.vhd
@@ -0,0 +1,60 @@
+-- -----------------------------------------------------------------------------
+--
+--  Title      :  FSMD implementation of GCD
+--             :
+--  Developers :  Jens Sparsø, Rasmus Bo Sørensen and Mathias Møller Bruhn
+--           :
+--  Purpose    :  This is a FSMD (finite state machine with datapath) 
+--             :  implementation the GCD circuit
+--             :
+--  Revision   :  02203 fall 2019 v.5.0
+--
+-- -----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity gcd is
+  port (clk : in std_logic;             -- The clock signal.
+    reset : in  std_logic;              -- Reset the module.
+    req   : in  std_logic;              -- Input operand / start computation.
+    AB    : in  unsigned(15 downto 0);  -- The two operands.
+    ack   : out std_logic;              -- Computation is complete.
+    C     : out unsigned(15 downto 0)); -- The result.
+end gcd;
+
+architecture fsmd of gcd is
+
+  type state_type is ( ... ); -- Input your own state names
+
+  signal reg_a, next_reg_a, next_reg_b, reg_b : unsigned(15 downto 0);
+
+  signal state, next_state : state_type;
+
+
+begin
+
+  -- Combinatoriel logic
+
+  cl : process (req,ab,state,reg_a,reg_b,reset)
+  begin
+
+    case (state) is
+
+        ...< combinatorical body > ...
+
+    end case;
+  end process cl;
+
+  -- Registers
+
+  seq : process (clk, reset)
+  begin
+
+    ...< register body >...
+
+  end process seq;
+
+
+end fsmd;
